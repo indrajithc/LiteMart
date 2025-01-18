1b0c4550300a725ffdb2582ba95ab1b6bd988858
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-01-18 21:31:07 +0530

    feat: sample home pages

---
+++ src/pages/[locale]/index.mjs
@@ -0,0 +1,5 @@
+const homePage = (params) => {
+  return `Home page with params: ${JSON.stringify(params)}`;
+};
+
+export default homePage;
---
+++ src/pages/[locale]/layout.mjs
@@ -0,0 +1,5 @@
+const localeBaseLayout = (children) => {
+  return `<html><head><title>Home Page</title></head><body><h1>${children}</h1></body></html>`;
+};
+
+export default localeBaseLayout;
