0dd6ac87f0b40bd4b1ebd778ad5574b197db202b
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-01-18 22:24:39 +0530

    refactor: api call added

--- src/widgets/Header/index.mjs
+++ src/widgets/Header/index.mjs
@@ -1,20 +1,21 @@
 const HeaderWidget = async (children) => {
   //  api logic here
   const response = await fetch(
-    "https://api.sampleapis.com/futurama/characters"
+    "https://raw.githubusercontent.com/indrajithc/LiteMart/refs/heads/main/__mocks__/api/navigation.json"
   );
 
-  const data = await response.json();
+  const { navigation } = await response.json();
 
   return `<header>
     <h1>Header</h1>
     <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+      ${navigation.map((link, index) => {
+        return `<li key=${index}>
+            <a href=${link.path}>${link.label}</a>
+          </li>`;
+      })}
       </ul>
-      ${JSON.stringify(data)}
     </nav>`;
 };
 
