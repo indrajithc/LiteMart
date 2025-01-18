d49737ef3cbaf805e1f0b1a7370feb1aa13c09b4
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-01-18 21:45:05 +0530

    refactor: header widget added

--- src/pages/[locale]/layout.mjs
+++ src/pages/[locale]/layout.mjs
@@ -1,5 +1,9 @@
-const localeBaseLayout = (children) => {
-  return `<html><head><title>Home Page</title></head><body><h1>${children}</h1></body></html>`;
+import HeaderWidget from "../../widgets/Header/index.mjs";
+
+const localeBaseLayout = async (children) => {
+  return `<html><head><title>Home Page</title></head><body>
+  ${await HeaderWidget()}
+  ${children}</body></html>`;
 };
 
 export default localeBaseLayout;
